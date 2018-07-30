5496fa83fbae6865ac691537e6675de50b146242
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-07-30 16:56:49 -0500

    add prototype code

--- app.js
+++ app.js
@@ -1,39 +1,78 @@
-var App = () => (
-  <div>
-    <h2>My Todo List</h2>
-    <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
-  </div>
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map((item, i) =>
+      <GroceryListItem key={i} item={item} />
+    )}
+  </ul>
 );
 
-class TodoListItem extends React.Component {
+class GroceryListItem extends React.Component {
   constructor(props) {
-    super(props);
+  	super(props);
 
-    // `state` is just an object literal
-    this.state = {
-      done: false
-    };
+  	this.state = {
+  		hover: false
+  	};
   }
 
-  // When a list item is clicked, we will toggle the `done`
-  // boolean, and our component's `render` method will run again
-  onListItemClick() {
-    this.setState({
-      done: !this.state.done
-    });
+  onListItemHover() {
+  	this.setState({
+  		hover: !this.state.hover
+  	});
   }
 
   render() {
-    // Making the style conditional on our `state` lets us
-    // update it based on user interactions.
     var style = {
-      textDecoration: this.state.done ? 'line-through' : 'none'
+    	fontWeight: this.state.hover ? 'bold' : 'normal'
     };
 
-    // You can pass inline styles using React's `style` attribute to any component
-    // snake-cased css properties become camelCased this this object
-    return (
-      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todo}</li>
-    );
+  	return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.item}</li>
+  	);
   }
 }
+
+ReactDOM.render(<GroceryList items={['peanut butter', 'kit-kats', 'ice cream', 'waffles', 'wine', 'black & milds']}/>, document.getElementById('app'));
+
+// var App = () => (
+//
+//   <div>
+//     <h2>My Todo List</h2>
+//     <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
+//   </div>
+// );
+//
+// class TodoListItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//
+//     // `state` is just an object literal
+//     this.state = {
+//       done: false
+//     };
+//   }
+//
+//   // When a list item is clicked, we will toggle the `done`
+//   // boolean, and our component's `render` method will run again
+//   onListItemClick() {
+//     this.setState({
+//       done: !this.state.done
+//     });
+//   }
+//
+//   render() {
+//     // Making the style conditional on our `state` lets us
+//     // update it based on user interactions.
+//     var style = {
+//       textDecoration: this.state.done ? 'line-through' : 'none'
+//     };
+//
+//     // You can pass inline styles using React's `style` attribute to any component
+//     // snake-cased css properties become camelCased this this object
+//     return (
+//       <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todo}</li>
+//     );
+//   }
+// }
+//
+// ReactDOM.render(<App />, document.getElementById("app"));
